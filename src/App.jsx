80485ba549567250306643185ff0e6f8aa465c39
RachelFairfax <fairfaxr1@gmail.com>
2025-05-12 17:26:37 +0100

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import './index.css';
 function App() {
 
   return (
-    <div ClassName={styles.wrapper}>
-      <Layout className={styles.content}>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Layout style={{flex: 1}}>
         <h1>Welcome to my Portfolio</h1>
         <p>This is the Home page!</p>
       </Layout>
