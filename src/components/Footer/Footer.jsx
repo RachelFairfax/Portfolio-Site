c0a8a527d5bf3fb2e57b07ae426273abb87ad6d7
RachelFairfax <fairfaxr1@gmail.com>
2025-06-10 00:02:19 +0100

    made homepage mobile-first

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.css';
 function Footer() {
     return (
     <footer className={styles.Footer}>
-        <p>&copy; 2025 Rachel Fairfax</p>
+        <p className={styles.FooterText}>&copy; 2025 Rachel Fairfax</p>
     </footer>)
 }
 
